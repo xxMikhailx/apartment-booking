03089deb9aa99dbbeca98ad432e1d578963dda8a
Mikhail Kaziuchyts <mikhail_kaziuchyts@epam.com>
2017-04-04 16:25:05 +0300

    Initial commit

---
+++ src/main/java/com/epam/apartment_booking/App.java
@@ -0,0 +1,13 @@
+package com.epam.apartment_booking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
